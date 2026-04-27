f84b3d191b61ff628b2c037c0e59895c0cb06d1e
S-Reuel <reuelsilva3110@gmail.com>
2026-04-27 15:18:56 -0300

    Remove whitespace

--- tests/test_divisao.c
+++ tests/test_divisao.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include"../src/divisao.h"
 #define ASSERT_EQ(expected, actual)\
-    if((expected)!=(actual)){\        
+    if((expected)!=(actual)){\
         printf("Falhou");\
         exit(1);\
     }else{\
